6440574ef019a11b807340944cb3b4d81b60bd14
Todor Toshev <t.toshev95@gmail.com>
2021-08-27 21:47:41 +0300

    Implemented commenting functionality.

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,3 +1,4 @@
+from blog.models import Comment
 from django import forms
 
 class EmailPostForm(forms.Form):
@@ -5,3 +6,8 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
