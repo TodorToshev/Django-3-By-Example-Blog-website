c722b90651b0334218ad14acc5804c518ad0edae
Todor Toshev <t.toshev95@gmail.com>
2021-08-29 17:11:56 +0300

    Added tagging functionality and retrieving posts by similarity.

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,28 +4,40 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
 
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
 
-    # return render(request, 'blog/post/list.html', {'posts': posts, "page": page})
+        '''Since this is a many-to-many relationship, you have to filter posts by
+        tags contained in a given list, which, in this case, contains only one
+        element. You use the __in field lookup.'''
+        object_list = object_list.filter(tags__in=[tag])
+        
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'posts': posts, "page": page, "tag": tag})
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = "posts"
+#     paginate_by = 3
+#     template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
@@ -43,7 +55,13 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+    # recommend similar posts:
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
--- src/blog_website/settings.py
+++ src/blog_website/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # custom
     "blog",
+    "taggit",
 ]
 
 MIDDLEWARE = [
